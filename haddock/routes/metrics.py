b2bbd5e609a4607ab4a982d47f5dcf7f6d604af5
Lorenzo Carneli <lorenzo.carneli@vitry94.org>
2022-11-17 11:03:06 +0100

    fix: fixed metrics route

--- haddock/routes/metrics.py
+++ haddock/routes/metrics.py
@@ -7,6 +7,6 @@ metrics_router = APIRouter()
 
 @metrics_router.post("/metrics", tags=["metrics"], response_model=Metrics, status_code=200)
 async def metrics(metrics: Metrics, request: Request):
-    if metrics.altitude and metrics.pressure and metrics.temperature:
+    if metrics.altitude is not None and metrics.pressure is not None and metrics.temperature is not None:
         request.app.state.prometheus_exporter.update(metrics)
     return metrics
